98feae3b27b09d4ebbeab95d12ec43f7e3719bff
Tiela Black-Law <tiela.rose@gmail.com>
2023-08-20 13:52:15 -0700

    Complete 771 Jewels and Stones

---
+++ 771_jewels_and_stones.py
@@ -0,0 +1,38 @@
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+# Example 1:
+
+# Input: jewels = "aA", stones = "aAAbbbb"
+# Output: 3
+# Example 2:
+
+# Input: jewels = "z", stones = "ZZ"
+# Output: 0
+ 
+# Constraints:
+
+# 1 <= jewels.length, stones.length <= 50
+# jewels and stones consist of only English letters.
+# All the characters of jewels are unique.
+
+# for loop
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels_set = set(jewels)
+        
+        num_jewels = 0
+        
+        for stone in stones:
+            if stone in jewels_set:
+                num_jewels += 1
+                
+        return num_jewels
+
+# list comprehension
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels_set = set(jewels)
+        
+        return sum(stone in jewels_set for stone in stones)
