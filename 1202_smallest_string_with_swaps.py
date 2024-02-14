a1291a19e89bcd73ee404ff8286f8b2d520f9d47
Tiela Black-Law <tiela.rose@gmail.com>
2024-02-13 22:03:00 -0800

    Complete 1202_smallest_string_with_swaps

---
+++ 1202_smallest_string_with_swaps.py
@@ -0,0 +1,73 @@
+# You are given a string s, and an array of pairs of indices in the string pairs where pairs[i] = [a, b] indicates 2 indices(0-indexed) of the string.
+
+# You can swap the characters at any pair of indices in the given pairs any number of times.
+
+# Return the lexicographically smallest string that s can be changed to after using the swaps.
+
+
+# Example 1:
+
+# Input: s = "dcab", pairs = [[0,3],[1,2]]
+# Output: "bacd"
+# Explaination:
+# Swap s[0] and s[3], s = "bcad"
+# Swap s[1] and s[2], s = "bacd"
+# Example 2:
+
+# Input: s = "dcab", pairs = [[0,3],[1,2],[0,2]]
+# Output: "abcd"
+# Explaination:
+# Swap s[0] and s[3], s = "bcad"
+# Swap s[0] and s[2], s = "acbd"
+# Swap s[1] and s[2], s = "abcd"
+# Example 3:
+
+# Input: s = "cba", pairs = [[0,1],[1,2]]
+# Output: "abc"
+# Explaination:
+# Swap s[0] and s[1], s = "bca"
+# Swap s[1] and s[2], s = "bac"
+# Swap s[0] and s[1], s = "abc"
+
+
+# Constraints:
+
+# 1 <= s.length <= 10^5
+# 0 <= pairs.length <= 10^5
+# 0 <= pairs[i][0], pairs[i][1] < s.length
+# s only contains lower case English letters.
+
+from collections import defaultdict
+
+
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        graph = defaultdict(list)
+
+        for x, y in pairs:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        def dfs(node, arr):
+            arr.append(node)
+            seen.add(node)
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    arr.append(neighbor)
+                    seen.add(neighbor)
+                    dfs(neighbor, arr)
+
+        seen = set()
+        s_list = [*s]
+
+        for i in range(len(s)):
+            if i not in seen:
+                indexes = []
+                dfs(i, indexes)
+                indexes.sort()
+                letters = [s[ind] for ind in indexes]
+                letters.sort()
+                for j in range(len(indexes)):
+                    s_list[indexes[j]] = letters[j]
+
+        return "".join(s_list)
