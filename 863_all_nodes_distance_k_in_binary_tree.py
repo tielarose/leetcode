b281b7483ce5a62d33c8c907df33a8968e216edc
Tiela Black-Law <tiela.rose@gmail.com>
2024-02-28 23:05:04 -0800

    Complete 863_all_nodes_distance_k_in_binary_tree

---
+++ 863_all_nodes_distance_k_in_binary_tree.py
@@ -0,0 +1,76 @@
+# Given the root of a binary tree, the value of a target node target, and an integer k, return an array of the values of all nodes that have a distance k from the target node.
+
+# You can return the answer in any order.
+
+
+# Example 1:
+
+
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, k = 2
+# Output: [7,4,1]
+# Explanation: The nodes that are a distance 2 from the target node (with value 5) have values 7, 4, and 1.
+# Example 2:
+
+# Input: root = [1], target = 1, k = 3
+# Output: []
+
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [1, 500].
+# 0 <= Node.val <= 500
+# All the values Node.val are unique.
+# target is the value of one of the nodes in the tree.
+# 0 <= k <= 1000
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque, defaultdict
+
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        # build a graph as if it was undirected
+        graph = defaultdict(list)
+
+        def dfs(node, parent):
+            if node and parent:
+                graph[node].append(parent)
+                graph[parent].append(node)
+
+            if node.left:
+                dfs(node.left, node)
+            if node.right:
+                dfs(node.right, node)
+
+        dfs(root, None)
+
+        # bfs starting at target
+        q = deque()
+        q.append(target)
+
+        distance_away = 0
+        seen = set()
+
+        while q and distance_away < k:
+            curr_level_len = len(q)
+
+            for _ in range(curr_level_len):
+                node = q.popleft()
+                seen.add(node)
+                for neighbor in graph[node]:
+                    if neighbor not in seen:
+                        q.append(neighbor)
+
+            distance_away += 1
+
+        ans = []
+        for node in q:
+            ans.append(node.val)
+
+        return ans
